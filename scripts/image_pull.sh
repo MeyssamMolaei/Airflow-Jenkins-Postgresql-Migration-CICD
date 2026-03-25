233421cd6d0db35a208b56e28a5c127c27d7164a
Meyssam Molaei <meyssam.molaei@gmail.com>
2026-03-25 15:56:21 +0100

    feat: add `image_pull.sh` script to pull essential container images using `ctr`.

--- scripts/image_pull.sh
+++ scripts/image_pull.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 images=(
-  "docker.io/apache/airflow:2.11.2"
-  "docker.io/prometheuscommunity/postgres-exporter:v0.19.1"
-  "docker.io/prom/statsd-exporter:v0.28.0"
-  "docker.io/prom/prometheus:v3.10.0"
-  "docker.io/grafana/grafana:12.4.1"
-  "docker.io/library/busybox:1.37.0"
-  "docker.io/jenkins/jenkins:2.541.3-lts-jdk21"
-  "docker.io/library/postgres:14.22-trixie"
+  "docker.io/apache/airflow:latest"
+  "docker.io/prometheuscommunity/postgres-exporter:latest"
+  "docker.io/prom/statsd-exporter:latest"
+  "docker.io/prom/prometheus:latest"
+  "docker.io/grafana/grafana:latest"
+  "docker.io/library/busybox:latest"
+  "docker.io/jenkins/jenkins:latest"
+  "docker.io/library/postgres:latest"
 )
 
 for image in "${images[@]}"; do
